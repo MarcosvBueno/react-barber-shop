15e55970c02072590c5df77565c9972cdb6a2ca8
MarcosvBueno <mvbueno07@gmail.com>
2024-03-03 01:15:44 -0300

    feat: update barbershop search component on desktop

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -25,19 +25,18 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
 
   return (
     <>
-    
-      <Header />
-      
-      <div className="lg:px-40 px-5 py-6 flex flex-col gap-6">
+      <div className="lg:container px-5 py-6 flex flex-col gap-6">
+      <div className="md:w-1/3">
       <Search defaultValues={{
         Search: searchParams?.search
       }}/>
+      </div>
         <h1 className="text-gray-400 font-bold text-xs lg:text-lg lg:text-white uppercase ">
           Resultados para &quot;{searchParams?.search}&quot;
         </h1>
-        <div className="grid grid-cols-2 mt-3 gap-4">
+        <div className="grid grid-flow-col mt-3 gap-4 w-full lg:max-w-[500px] ">
           {barbershops.map((barbershop) => (
-            <div key={barbershop.id} className="w-full lg:max-w-[350px] ">
+            <div key={barbershop.id} className="w-full lg:max-w-[300px] ">
               <BarbershopItem barbershop={barbershop} />
             </div>
           ))}
